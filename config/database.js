4c068ad9b91defd2e3107e091397818306f2effe
MilkWind <2250957906@qq.com>
2025-05-31 21:18:09 +0800

    fix parameters

--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+const {mongoUri} = require('./config')
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error('Database connection error:', error);
-    process.exit(1);
-  }
+    try {
+        const conn = await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error('Database connection error:', error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB; 
